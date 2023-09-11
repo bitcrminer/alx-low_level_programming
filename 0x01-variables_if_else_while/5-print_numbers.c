2b57ecee465dcba531bc98295bb879233c1d5892
bitcrminer <anthony.augwu@gmail.com>
2023-09-11 04:02:05 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabet in lowercase and upper case
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+	return (0);
+}
+
+
